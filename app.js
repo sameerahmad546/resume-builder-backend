f75632005582d7328f0cf68523b1069270d1a377
Sameer Ahmad <sameerah546@gmail.com>
2023-11-06 16:19:06 +0500

    setup cors

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const cors = require('cors')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors())
+app.use(cors({
+    origin: '*',
+    methods: 'GET,POST'
+}))
 
 db.connect()
 
